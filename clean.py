92e350c4e5c44c51f39bc5b0d02a7b079a9c7b15
andreyanch <70206870+andreyanch@users.noreply.github.com>
2020-09-04 16:39:49 +0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 import os
 
-clean_container = 'docker rm -f `docker ps -a | grep \'calculator\' | awk \'{print $1;}\'`'
+clean_container = 'docker rm -f $(docker ps -a -q)'`'
 print(str(os.system (clean_container)))
 
-clean_untagged_image = 'docker rmi `docker images | grep \'calculator\' | awk \'{ print $3; }\'`'
+clean_untagged_image = 'docker volume rm $(docker volume ls -q)'`'
 print(str(os.system(clean_untagged_image)))
