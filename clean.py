70624bc3d6098f9f6a53cff97b20312b7611179f
andreyanch <70206870+andreyanch@users.noreply.github.com>
2020-09-04 16:48:31 +0500

    Delete clean.py

--- clean.py
+++
@@ -1,7 +0,0 @@
-import os
-
-clean_container = 'docker rm -f $(docker ps -a -q)'`'
-print(str(os.system (clean_container)))
-
-clean_untagged_image = 'docker volume rm $(docker volume ls -q)'`'
-print(str(os.system(clean_untagged_image)))
