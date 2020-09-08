66244fb7f8bd9a5f810b72145c02fc49f4879e0a
andreyanch <70206870+andreyanch@users.noreply.github.com>
2020-09-08 11:23:21 +0500

    Update calculatot.py

--- calculatot.py
+++ calculatot.py
@@ -1,34 +1,15 @@
-#!flask/bin/python
 from flask import Flask
+
 from flask import request
-import datetime
-import re
-import pytz
+
 
 app = Flask(__name__)
 
-print("Ноль в качестве знака операции"
-      "\nзавершит работу программы")
-while True:
-    s = input("Знак (+,-,*,/): ")
-    if s == '0':
-        break
-    if s in ('+', '-', '*', '/'):
-        x = float(input("x="))
-        y = float(input("y="))
-        if s == '+':
-            print("%.2f" % (x+y))
-        elif s == '-':
-            print("%.2f" % (x-y))
-        elif s == '*':
-            print("%.2f" % (x*y))
-        elif s == '/':
-            if y != 0:
-                print("%.2f" % (x/y))
-            else:
-                print("Деление на ноль!")
-    else:
-        print("Неверный знак операции!")
+
+@app.route("/")
+def home():
+    return "Home Page"
 
 
-app.run(host='0.0.0.0', port=5000)
+if (__name__ == "__main__"):
+    app.run(debug=True)
