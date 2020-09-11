d9c0c8e1d03e92356c3309a02fbf5345c044e82e
andreyanch <70206870+andreyanch@users.noreply.github.com>
2020-09-11 14:55:59 +0500

    Update calculatot.py

--- calculatot.py
+++ calculatot.py
@@ -1,18 +1,15 @@
 from flask import Flask
-
 from flask import request
-
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return "Home Page"
+    return request.args.get("a")
 
 
 if (__name__ == "__main__"):
     app.run(debug=True)
-    
-    
+
 app.run(host='0.0.0.0', port=5000)
